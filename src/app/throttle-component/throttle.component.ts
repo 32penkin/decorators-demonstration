a817995b6f026f41bdeb69b27ff65fad9b846d10
Yauhen <penkin.evgeniy@gmail.com>
2017-05-27 17:17:14 +0300

    feat (throttle component): throttle decorator demonstration added

--- src/app/throttle-component/throttle.component.ts
+++ src/app/throttle-component/throttle.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { throttle } from '../../../node_modules/try-decorators/src/throttle.decorator';
 
 @Component({
   selector: 'throttle',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./throttle.component.css']
 })
 export class Throttle {
+  count: number = 0;
   
+  @throttle(2000)
+  doubleCount() {
+    this.count = this.count + 2;
+  }
 }
