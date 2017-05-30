567c169b51ced18d4d26be7391d2f6eeb7104af3
Yauhen <penkin.evgeniy@gmail.com>
2017-05-30 14:26:05 +0300

    feat (cache component): cache decoration demonstration

--- src/app/cache-component/cache.component.ts
+++ src/app/cache-component/cache.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { cacheDecorator, timingDecorator } from 'try-decorators';
 
 @Component({
@@ -6,18 +6,22 @@ import { cacheDecorator, timingDecorator } from 'try-decorators';
   templateUrl: './cache.component.html',
   styleUrls: ['./cache.component.css']
 })
-export class Cache {
+export class Cache implements OnInit {
   num: number;
   arr: any[] = [];
+
+  ngOnInit(): void {
+    document.addEventListener('log', (data: CustomEvent) => {
+     this.arr.push(data.detail);
+    }, false);
+  }
  
-  @timingDecorator
   @cacheDecorator
   fact(n) {
     let tot = 1;
     for(let i = 2; i <= n; i++) {
       tot *= i;
     }
-    this.arr.push(tot);
     return tot;
   }
 
